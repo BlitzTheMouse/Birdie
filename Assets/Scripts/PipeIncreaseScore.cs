ba5250e0e7c005e3e07e80145fadd59ee2cb00dd
BlitzTheMouse <fvillarreal2012@gmail.com>
2025-05-21 23:22:24 -0500

    scene stuff

--- Assets/Scripts/PipeIncreaseScore.cs
+++ Assets/Scripts/PipeIncreaseScore.cs
@@ -7,10 +7,12 @@ public class PipeIncreaseScore : MonoBehaviour
     [SerializeField] private AudioClip collectSound;
 
     private AudioSource audioSource;
+    private Collider2D triggerCollider;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        triggerCollider = GetComponent<Collider2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -21,6 +23,14 @@ public class PipeIncreaseScore : MonoBehaviour
 
             if (collectSound != null && audioSource != null)
                 audioSource.PlayOneShot(collectSound);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && triggerCollider != null)
+        {
+            triggerCollider.enabled = false;
         }
     }
 }
